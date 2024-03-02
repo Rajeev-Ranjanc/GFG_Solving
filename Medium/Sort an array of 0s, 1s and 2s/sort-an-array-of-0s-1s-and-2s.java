788422b3c41db06563bb6054e6e6f57cc93d237d
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-03-02 16:07:56 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.java
+++ Medium/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.java
@@ -13,7 +13,34 @@ class Solution
     public static void sort012(int a[], int n)
     {
         // code here 
-        Arrays.sort(a);
+        // Arrays.sort(a);
+        int zero = 0;
+        int one = 1;
+        int two = 0;
+        int x= 0;
+        for(int i=0;i<n;i++){
+            if(a[i] == 0){
+                zero++;
+            }
+            else if(a[i] == 1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        
+        for (int i = 0; i < zero; i++) {
+            a[i] = 0;
+        }
+        for (int i = zero; i < zero + one - 1; i++) {
+            a[i] = 1;
+        }
+        for (int i = zero + one - 1; i < n; i++) {
+            a[i] = 2;
+        }
+
+        
     }
 }
 
