6b95dd67a8c82b38cb4162bd030eddef18677b3d
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-03 16:53:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Common elements/common-elements.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        // code here 
+        ArrayList<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> first = new HashMap<>();
+        Map<Integer, Integer> second = new HashMap<>();
+
+        for (int num : A) {
+            first.put(num, first.getOrDefault(num, 0) + 1);
+        }
+        for (int num : B) {
+            second.put(num, second.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : C) {
+            if (first.containsKey(num) && second.containsKey(num)) {
+                if (!ans.contains(num)) {
+                    ans.add(num);
+                }
+            }
+        }
+        return ans;
+    }
+}
