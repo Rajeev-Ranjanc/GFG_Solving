75e30d71d1f6000c04d8ea172e730b00871dfe90
Rajeev Ranjan <68611855+Rajeev-Ranjanc@users.noreply.github.com>
2024-04-19 02:28:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotate a 2D array without using extra space/rotate-a-2d-array-without-using-extra-space.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * n + j]);
+                }
+            }
+
+            new Solution().rotateMatrix(arr, n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    out.print(arr[i][j] + " ");
+                }
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    void rotateMatrix(int matrix[][], int n) {
+        // code here
+         //revrsing the array(row) elements 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+        
+        //doing transpose of the matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                 int temp = matrix[i][j];
+                 matrix[i][j] = matrix[j][i];
+                 matrix[j][i] = temp;
+
+            }
+        }
+        
+    }
+}
